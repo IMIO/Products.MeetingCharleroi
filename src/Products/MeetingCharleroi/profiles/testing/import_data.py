9130a78cc57e9e425f676caf58ce56715dd6a327
Gauthier Bastien <g.bastien@imio.be>
2016-11-23 09:45:07 +0000

    Use same config for categories then PM testing profile as it is tested

--- src/Products/MeetingCharleroi/profiles/testing/import_data.py
+++ src/Products/MeetingCharleroi/profiles/testing/import_data.py
@@ -90,18 +90,16 @@ template2 = ItemTemplateDescriptor(id='template2',
 
 
 # Categories -------------------------------------------------------------------
-categories = [
-    CategoryDescriptor('deployment', 'Deployment topics'),
-    CategoryDescriptor('maintenance', 'Maintenance topics'),
-    CategoryDescriptor('development', 'Development topics'),
-    CategoryDescriptor('events', 'Events'),
-    CategoryDescriptor('research', 'Research topics'),
-    CategoryDescriptor('projects', 'Projects'),
-    # A vintage category
-    CategoryDescriptor('marketing', 'Marketing', active=False),
-    # usingGroups category
-    CategoryDescriptor('subproducts', 'Subproducts wishes', usingGroups=('vendors',)),
-]
+deployment = CategoryDescriptor('deployment', 'Deployment topics')
+maintenance = CategoryDescriptor('maintenance', 'Maintenance topics')
+development = CategoryDescriptor('development', 'Development topics')
+events = CategoryDescriptor('events', 'Events')
+research = CategoryDescriptor('research', 'Research topics')
+projects = CategoryDescriptor('projects', 'Projects')
+# A vintage category
+marketing = CategoryDescriptor('marketing', 'Marketing', active=False)
+# usingGroups category
+subproducts = CategoryDescriptor('subproducts', 'Subproducts wishes', usingGroups=('vendors',))
 
 # Users and groups -------------------------------------------------------------
 pmFinController = UserDescriptor('pmFinController', [])
@@ -232,7 +230,7 @@ collegeMeeting.assembly = 'Pierre Dupont - Bourgmestre,\n' \
                           'Jacqueline Exemple, Responsable du CPAS'
 collegeMeeting.signatures = 'Pierre Dupont, Bourgmestre - Charles Exemple, Secrétaire communal'
 collegeMeeting.certifiedSignatures = []
-collegeMeeting.categories = categories
+collegeMeeting.categories = [development, research]
 collegeMeeting.shortName = 'College'
 collegeMeeting.meetingFileTypes = [annexe, annexeBudget, annexeCahier, itemAnnex,
                                    annexeDecision, overheadAnalysis, marketingAnalysis,
@@ -320,7 +318,8 @@ councilMeeting.meetingManagers = ['pmManager', ]
 councilMeeting.assembly = 'Default assembly'
 councilMeeting.signatures = 'Default signatures'
 councilMeeting.certifiedSignatures = []
-councilMeeting.categories = categories
+councilMeeting.categories = [deployment, maintenance, development, events,
+                             research, projects, marketing, subproducts]
 councilMeeting.shortName = 'Council'
 councilMeeting.meetingFileTypes = [annexe, annexeBudget, annexeCahier,
                                    itemAnnex, annexeDecision, adviceAnnex, adviceLegalAnalysis]
