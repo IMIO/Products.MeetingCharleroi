cd47d7ef12a29738eff975452433298793459e02
Gauthier Bastien <g.bastien@imio.be>
2017-03-13 16:10:18 +0000

    PEP8

--- src/Products/MeetingCharleroi/adapters.py
+++ src/Products/MeetingCharleroi/adapters.py
@@ -76,7 +76,8 @@ customWfAdaptations = ('no_publication', 'no_global_observation',
                        'only_creator_may_delete',
                        'pre_validation', 'items_come_validated',
                        'return_to_proposing_group', 'charleroi_add_refadmin',
-                       'waiting_advices', 'postpone_next_meeting', 'mark_not_applicable', 'removed')
+                       'waiting_advices', 'postpone_next_meeting',
+                       'mark_not_applicable', 'removed')
 MeetingConfig.wfAdaptations = customWfAdaptations
 originalPerformWorkflowAdaptations = adaptations.performWorkflowAdaptations
 
