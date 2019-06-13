e9a6a22f7cad850bba4ceabb5dd1c6d2be6a103b
Gauthier Bastien <g.bastien@imio.be>
2019-06-13 17:33:14 +0200

    Launch MeetingCommunes migration then reapply MeetingCharleroi or migration is launched twice because of call to upgradeAll

--- src/Products/MeetingCharleroi/migrations/migrate_to_4_1.py
+++ src/Products/MeetingCharleroi/migrations/migrate_to_4_1.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-
+from Products.GenericSetup.tool import DEPENDENCY_STRATEGY_NEW
 from Products.MeetingCommunes.migrations.migrate_to_4_1 import Migrate_To_4_1 as MCMigrate_To_4_1
 
 import logging
@@ -14,7 +14,10 @@ class Migrate_To_4_1(MCMigrate_To_4_1):
     def run(self):
         # reapply the actions.xml of collective.iconifiedcategory
         self.ps.runImportStepFromProfile('profile-collective.iconifiedcategory:default', 'actions')
-        MCMigrate_To_4_1.run(self, profile_name=u'profile-Products.MeetingCharleroi:default')
+        super(Migrate_To_4_1, self).run()
+        self.reinstall(profiles=[u'profile-Products.MeetingCharleroi:default'],
+                       ignore_dependencies=False,
+                       dependency_strategy=DEPENDENCY_STRATEGY_NEW)
 
 
 def migrate(context):
